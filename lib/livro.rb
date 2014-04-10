1ee604261f83c76321494cd3606d5a5fc5eba5c1
Vitor Cassiano <virjcas@gmail.com>
2014-04-10 00:39:46 -0300

    Some writes for the class Livro

--- lib/livro.rb
+++ lib/livro.rb
@@ -1,7 +1,29 @@
 # coding: UTF-8
-
 class Livro
-	def initialize(autor, isbn = "1", numero_de_paginas)
-		puts  "Autor: #{autor}, ISBN: #{isbn}, Pág: #{numero_de_paginas}"
+	attr_writer :preco, :autor
+	attr_reader :preco, :autor
+	attr_accessor :preco, :autor
+
+
+
+	def initialize(autor, isbn = "1", numero_de_paginas, preco)
+		@autor = autor
+		@isbn = isbn
+		@numero_de_paginas = numero_de_paginas
+		@preco = preco
 	end
+
+	def to_s
+		puts  "Autor: #{@autor}, ISBN: #{@isbn}, Pág: #{@numero_de_paginas}"
+	end
+
+=begin
+	def preco
+		@preco
+	end
+
+	def preco=(preco)
+		@preco = preco
+	end
+=end
 end
